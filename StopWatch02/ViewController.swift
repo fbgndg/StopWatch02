194985fcd49d65a8cc705a83ad4d2b28d1520321
D7703_19 <d7703_19@d7703-19ui-imac.local>
2019-04-04 15:33:14 +0900

    .

--- StopWatch02/ViewController.swift
+++ StopWatch02/ViewController.swift
@@ -9,12 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startB: UIBarButtonItem!
+    @IBOutlet weak var pauseB: UIBarButtonItem!
+    @IBOutlet weak var resetB: UIBarButtonItem!
+    var time = 0
+    //타이머 객체 생성
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timeLabel.text = String(time)
+        
     }
-
+    @IBAction func startBtnPressed(_ sender: Any) {
+        if (myTimer.isValid) {
+            return;
+ }
+        
+        
+        
+        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+        startB.isEnabled = false
+        
+        changeButtonState(start: false, puase: true, stop: true)
+    }
+        /*time = time + 1
+        timeLabel.text = String(time)
+    }*/
+    
+    @IBAction func pauseBtnPressed(_ sender: Any) {
+    //타이머가 정지 제개 가능
+        myTimer.invalidate()
+        
+        changeButtonState(start: true, puase: false, stop: true)
+    }
+    
+    @IBAction func resetBtnPressed(_ sender: Any) {
+    
+        myTimer.invalidate()
+        time=0
+        timeLabel.text = String(time)
+        
+        changeButtonState(start: true, puase: false, stop: false)
+    }
+    
+    @objc func updateTimer(){
+        time+=1
+        timeLabel.text = String(time)
+    }
+    
+        func changeButtonState(start: Bool, puase: Bool, stop:Bool) {
+            startB.isEnabled = start
+            pauseB.isEnabled = puase
+            resetB.isEnabled = stop
+        }
 
 }
-
